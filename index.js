b0131cad0047fbeb068bc087b0dac9a3de118078
Praveennaik6 <praveennaik0814@gmail.com>
2025-04-28 17:57:29 +0530

    adding routers

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
-const app=express();
+const express=require("express");
+const {userRouter}=require("./routes/user");
+const {courseRouter}=require("./routes/course")
+const app=express()
 
-app.post("/user/signup", function(req, res){
-    res.json({
-        message:"signup  endpoint"
-    })
-})
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/course", courseRouter);
 
-app.post("/user/signin", function(req, res){
-    res.json({
-        message:"signup endpoint"
-    })
-})
+app.listen(3000);
 
-app.post("/user/purchase", function(req, res){
-    res.json({
-        message:"signup endpoint"
-    })
-})
-
-app.post("/course/purchase", function(req, res){
-    res.json({
-        message:"signup endpoint"
-    })
-})
-
-app.post("/courses", function(req, res){
-    res.json({
-        message:"signup endpoint"
-    })
-})
