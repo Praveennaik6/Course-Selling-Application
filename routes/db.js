9e6572f108b9b659f85bba77ebff5e128b614246
Praveennaik6 <praveennaik0814@gmail.com>
2025-04-29 17:52:34 +0530

    adding Schema

--- routes/db.js
+++ routes/db.js
@@ -1,18 +1,33 @@
-const{Schema, default: mongoose}=require("mongoose")
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
 
 const userSchema=Schema({
-
+    email:{type:String, unique:true},
+    password:String,
+    firstName:String,
+    lastName:String,
 });
 
 const adminSchema=Schema({
+    email:{type:String, unique:true},
+    password:String,
+    firstName:String,
+    lastName:String,
 
 });
 
 const courseSchema=Schema({
-
+    title:String,
+    description:String,
+    price:Number,
+    imageUrl:String,
+    creatorId:ObjectId,
 });
 
 const purchaseSchema=Schema({
+    userId:ObjectId,
+    courseId:ObjectId
 
 });
 
@@ -20,3 +35,11 @@ const userModel=mongoose.Model("user", userSchema);
 const adminModel=mongoose.Model("admin",adminSchema);
 const courseModel=mongoose.Model("course", courseSchema);
 const purchaseModel=mongoose.Model("purcahse", purchaseSchema);
+
+module.export={
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+
+}
